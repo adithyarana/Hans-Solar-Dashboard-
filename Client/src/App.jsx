1a22f28c2035f0ea6e97a0fe6a4c7792fd46b78c
adithya <adithyarana447@gmail.com>
2025-08-20 20:11:12 +0530

    frontend setup

---
+++ Client/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-2xl font-bold h-screen flex items-center justify-center'>Uday bhai aapko chal rha hai</div>
+    </>
+  )
+}
+
+export default App
